e65bd2abb56a865faab54b08ed3a7cdc26974f7e
Артём <@DESKTOP-1MI1908>
2024-04-14 20:47:20 +0300

    Добавьте файлы проекта.

---
+++ ABBA/Purchases.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ABBA.WILDBERDataSetTableAdapters;
+
+namespace ABBA
+{
+
+    public partial class Purchases : Page
+    {
+        GOODTableAdapter go = new GOODTableAdapter();
+        public Purchases()
+        {
+            InitializeComponent();
+            ColourDataGrid.ItemsSource = go.GetData();
+        }
+    }
+}
